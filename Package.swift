f30211ef4abfc7129583521280a6acb2c988a3f4
Kare Morstol <kare@nottoobadsoftware.com>
2019-06-21 19:20:16 +0200

    Parse Unicode scripts property file using FootlessParser

--- Package.swift
+++ Package.swift
@@ -4,28 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "TextPicker",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "TextPicker",
-            targets: ["TextPicker"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "TextPicker",
-            dependencies: []),
-        .testTarget(
-            name: "TextPickerTests",
-            dependencies: ["TextPicker"]),
-				.target(
-					name: "UnicodeBootstrap",
-					dependencies: []),
-    ]
+	name: "TextPicker",
+	products: [
+		// Products define the executables and libraries produced by a package, and make them visible to other packages.
+		.library(
+			name: "TextPicker",
+			targets: ["TextPicker"]),
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		.package(url: "https://github.com/kareman/FootlessParser", from: "0.5.2"),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
+		.target(
+			name: "TextPicker",
+			dependencies: []),
+		.testTarget(
+			name: "TextPickerTests",
+			dependencies: ["TextPicker"]),
+		.target(
+			name: "UnicodeBootstrap",
+			dependencies: ["FootlessParser"]),
+	]
 )
