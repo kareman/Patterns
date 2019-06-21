58861a3094c8f222b572d02c503ce99604b1df8d
Kare Morstol <kare@nottoobadsoftware.com>
2019-06-21 13:36:54 +0200

    Create UnicodeBootstrap executable

--- Package.swift
+++ Package.swift
@@ -24,5 +24,8 @@ let package = Package(
         .testTarget(
             name: "TextPickerTests",
             dependencies: ["TextPicker"]),
+				.target(
+					name: "UnicodeBootstrap",
+					dependencies: []),
     ]
 )
