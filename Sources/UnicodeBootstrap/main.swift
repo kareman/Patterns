42de87b9a098359213865adbf07f763f1dcdfe89
Kare Morstol <kare@nottoobadsoftware.com>
2019-06-21 22:30:40 +0200

    unicodeProperty

--- Sources/UnicodeBootstrap/main.swift
+++ Sources/UnicodeBootstrap/main.swift
@@ -11,26 +11,25 @@ public func parse<A, S: StringProtocol>(_ p: Parser<Character, A>, _ s: S) throw
 	return try (p <* eof()).parse(AnyCollection(s)).output
 }
 
-let hex = char(CharacterSet(charactersIn: "0" ... "9").union(CharacterSet(charactersIn: "A" ... "F")), name: "hex")
-
-let hex4 = { UInt32($0, radix: 16)! } <^> count(4 ... 5, hex)
-
-let rhex4 = curry { $0 ... $1 } <^> hex4 <* string("..") <*> hex4
-
-let rorhex4 = rhex4 <|> { $0 ... $0 } <^> hex4
-
-let v = tuple <^> rorhex4 <* oneOrMore(whitespace) <* string("; ") <*> oneOrMore(not(" "))
-
-let l = v <* oneOrMore(any()) <* eof()
+func unicodeProperty<S: StringProtocol>(fromDataFile text: S) -> [(range: ClosedRange<UInt32>, property: String)] {
+	let hex = char(CharacterSet(charactersIn: "0" ... "9").union(CharacterSet(charactersIn: "A" ... "F")), name: "hex")
+	let hex4 = { UInt32($0, radix: 16)! } <^> count(4 ... 5, hex)
+	let rhex4 = curry { $0 ... $1 } <^> hex4 <* string("..") <*> hex4
+	let rorhex4 = rhex4 <|> { $0 ... $0 } <^> hex4
+	let v = tuple <^> rorhex4 <* oneOrMore(whitespace) <* string("; ") <*> oneOrMore(not(" "))
+	let l = v <* oneOrMore(any())
+	return (text.split(separator: "\n"))
+		.compactMap {
+			try? parse(l, $0)
+		}
+}
 
 do {
 	let scripts = try String(contentsOf: getLocalURL(for: "Scripts.txt"))
 
-	let result = (scripts.split(separator: "\n") as [Substring]).compactMap {
-		try? parse(l, $0)
-	}
-	print(result)
+	let result = unicodeProperty(fromDataFile: scripts)
 
+	print(result)
 } catch {
 	print(error)
 }
