a4d997d53b34767e6755fdaa27864a43d2587964
Kare Morstol <kare@nottoobadsoftware.com>
2019-06-22 23:33:27 +0200

    Make generic and add error handling

--- Sources/UnicodeBootstrap/main.swift
+++ Sources/UnicodeBootstrap/main.swift
@@ -11,7 +11,8 @@ public func parse<A, S: StringProtocol>(_ p: Parser<Character, A>, _ s: S) throw
 	return try (p <* eof()).parse(AnyCollection(s)).output
 }
 
-func unicodeProperty<S: StringProtocol>(fromDataFile text: S) -> [(range: ClosedRange<UInt32>, property: String)] {
+func unicodeProperty<S: StringProtocol>(fromDataFile text: S)
+	-> [(range: ClosedRange<UInt32>, property: String)] {
 	let hex = char(CharacterSet(charactersIn: "0" ... "9").union(CharacterSet(charactersIn: "A" ... "F")), name: "hex")
 	let hex4 = { UInt32($0, radix: 16)! } <^> count(4 ... 5, hex)
 	let rhex4 = curry { $0 ... $1 } <^> hex4 <* string("..") <*> hex4
@@ -48,8 +49,18 @@ func caseName(_ string: String) -> String {
 }
 
 do {
-	let scripts = try String(contentsOf: getLocalURL(for: "WordBreakProperty.txt"))
-	let properties = Dictionary(grouping: unicodeProperty(fromDataFile: scripts), by: { $0.property })
+	guard CommandLine.arguments.count == 2 else {
+		print("""
+
+			Converts Unicode property data files to Swift code.
+
+			    Usage: unicode_properties <filepath>
+
+			""")
+		exit(1)
+	}
+	let unicodeData = try String(contentsOfFile: CommandLine.arguments[1])
+	let properties = Dictionary(grouping: unicodeProperty(fromDataFile: unicodeData), by: { $0.property })
 		.mapValues { ranges -> [ClosedRange<UInt32>] in
 			ranges.map { $0.range }
 				.sorted { $0.lowerBound < $1.lowerBound }
@@ -60,18 +71,18 @@ do {
 	}
 
 	print()
-	print("enum UnicodeScript: String {")
+	print("enum UnicodeProperty: String {")
 	print("  case", properties.keys.map { #"\#(caseName($0)) = "\#($0)""# }.joined(separator: ", "))
 	print("}")
 
 	print()
-	print("let scriptRanges: [UnicodeScript : ContiguousArray<ClosedRange<UInt32>>] = [")
-	properties.forEach { scriptName, ranges in
-		print("  .\(caseName(scriptName)): [", ranges.map {"\($0)"}.joined(separator: ", "), "],", separator: "")
+	print("let propertyRanges: [UnicodeProperty : ContiguousArray<ClosedRange<UInt32>>] = [")
+	properties.forEach { propertyName, ranges in
+		print("  .\(caseName(propertyName)): [", ranges.map {"\($0)"}.joined(separator: ", "), "],", separator: "")
 	}
 	print("]")
 	print()
 } catch {
-	print(error)
+	print(error.localizedDescription)
+	exit(Int32(error._code))
 }
-
