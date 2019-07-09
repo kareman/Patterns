46f1b57705b33e3cf787192953d97ecaa9c6b313
Kare Morstol <kare@nottoobadsoftware.com>
2019-07-09 15:21:17 +0200

    Include performance tests on Linux

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,10 +2,12 @@
 import XCTest
 
 @testable import TextPickerTests
+@testable import PerformanceTests
 
 let tests: [XCTestCaseEntry] = [
 	testCase(TextPatternTests.allTests),
 	testCase(PatternsTests.allTests),
+	testCase(PerformanceTests.allTests),
 ]
 
 XCTMain(tests)
