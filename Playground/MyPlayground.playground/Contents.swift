55bed38e0d1228a7a87a14eb9e014c70b98589df
Kare Morstol <kare@nottoobadsoftware.com>
2020-06-15 20:43:16 +0200

    Playground parser view (#36)
    * Add playground example screenshot to readme.

--- Playground/MyPlayground.playground/Contents.swift
+++ Playground/MyPlayground.playground/Contents.swift
@@ -4,3 +4,31 @@
 // ℹ️ Please restart Xcode if autocomplete is not working.
 
 import Patterns
+import PlaygroundSupport
+import SwiftUI
+
+let text = """
+I can eat glass and it doesn't hurt me.
+ᛖᚴ ᚷᛖᛏ ᛖᛏᛁ ᚧ ᚷᛚᛖᚱ ᛘᚾ ᚦᛖᛋᛋ ᚨᚧ ᚡᛖ ᚱᚧᚨ ᛋᚨᚱ
+Ek get etið gler án þess að verða sár.
+Eg kan eta glas utan å skada meg.
+ᛁᚳ᛫ᛗᚨᚷ᛫ᚷᛚᚨᛋ᛫ᛖᚩᛏᚪᚾ᛫ᚩᚾᛞ᛫ᚻᛁᛏ᛫ᚾᛖ᛫ᚻᛖᚪᚱᛗᛁᚪᚧ᛫ᛗᛖ᛬
+Μπορώ να φάω σπασμένα γυαλιά χωρίς να πάθω τίποτα.
+我能吞下玻璃而不伤身体。
+我能吞下玻璃而不傷身體。
+Góa ē-tàng chia̍h po-lê, mā bē tio̍h-siong.
+私はガラスを食べられます。それは私を傷つけません。
+나는 유리를 먹을 수 있어요. 그래도 아프지 않아요
+काचं शक्नोम्यत्तुम् । नोपहिनस्ति माम् ॥
+"""
+
+let p = Capture(name: ">=6", letter.repeat(6...))
+	/ Capture(name: "4...5", letter.repeat(4 ... 5))
+	/ Capture(name: "2...3", letter.repeat(2 ... 3))
+	/ Capture(name: "1", letter)
+
+let view = try ParserView(text: text, pattern: p)
+let size = NSSize(width: 600, height: 600)
+let hosting = NSHostingController(rootView: view)
+hosting.view.frame.size = size
+PlaygroundPage.current.setLiveView(hosting)
---
+++ Playground/MyPlayground.playground/Sources/General.swift
@@ -0,0 +1,90 @@
+//
+//  General.swift
+//  PlaygroundView
+//
+//  Created by Kåre Morstøl on 13/06/2020.
+//
+
+import AppKit
+import Patterns
+
+typealias Attributes = [NSAttributedString.Key: Any]
+
+let defaultTextAttributes: Attributes = {
+	let defaultTextSize = CGFloat(14)
+	let font = NSFont(name: "Menlo", size: defaultTextSize)
+		?? NSFont.monospacedDigitSystemFont(ofSize: defaultTextSize, weight: .regular)
+	return [.font: font, .foregroundColor: NSColor.textColor, .backgroundColor: NSColor.textBackgroundColor]
+}()
+
+let defaultCapturedAttributes =
+	[NSColor.systemGreen, .systemPurple, .systemOrange, .systemTeal, .systemBlue, .systemRed]
+	.map { color -> Attributes in
+		var attribs = defaultTextAttributes
+		attribs[.backgroundColor] = color.withAlphaComponent(0.5)
+		return attribs
+	}
+
+func adorn<S: Sequence>(_ string: String, matches: S) -> (NSMutableAttributedString, [String: Attributes])
+	where S.Element == Parser<String>.Match {
+	var capturedAttributes = defaultCapturedAttributes.repeatForever().makeIterator()
+	let attributedString = NSMutableAttributedString(string: string, attributes: defaultTextAttributes)
+	var captureColors = [String: Attributes]()
+	for match in matches {
+		for (name, range) in match.captures {
+			let nsrange = NSRange(range, in: string)
+			if captureColors[name ?? "unnamed"] == nil { captureColors[name ?? "unnamed"] = capturedAttributes.next()! }
+			let attributes = captureColors[name ?? "unnamed"]
+			attributedString.setAttributes(attributes, range: nsrange)
+		}
+	}
+
+	return (attributedString, captureColors)
+}
+
+extension Sequence {
+	func repeatForever() -> LazySequence<UnfoldSequence<Element, Iterator>> {
+		sequence(state: self.makeIterator()) { (iterator: inout Iterator) -> Element? in
+			iterator.next() ?? {
+				iterator = self.makeIterator()
+				return iterator.next()
+			}()
+		}.lazy
+	}
+}
+
+extension NSAttributedString {
+	var nsRange: NSRange { NSRange(location: 0, length: self.length) }
+}
+
+import AppKit
+import SwiftUI
+
+struct Label: NSViewRepresentable {
+	typealias TheUIView = NSTextView
+	var configuration = { (view: TheUIView) in }
+
+	func makeNSView(context: Context) -> TheUIView {
+		let view = TheUIView()
+		view.isEditable = false
+		return view
+	}
+
+	func updateNSView(_ nsView: TheUIView, context: Context) {
+		configuration(nsView)
+	}
+}
+
+struct SingleLineLabel: NSViewRepresentable {
+	typealias TheUIView = NSTextField
+	let content: NSAttributedString
+
+	func makeNSView(context: Context) -> TheUIView {
+		let view = TheUIView(labelWithAttributedString: content)
+		view.isEditable = false
+		//view.alignment = .center
+		return view
+	}
+
+	func updateNSView(_ nsView: TheUIView, context: Context) {}
+}
