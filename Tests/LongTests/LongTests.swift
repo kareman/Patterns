0c5192acd0af42249eb8fb2e4c3888e5be547a94
Kare Morstol <kare@nottoobadsoftware.com>
2020-06-20 19:42:28 +0200

    Add testPlaygroundExample.

--- Tests/LongTests/LongTests.swift
+++ Tests/LongTests/LongTests.swift
@@ -20,4 +20,32 @@ class LongTests: XCTestCase {
 			type(of: "a" • &&letter • ascii • "b") == Concat<Literal, Concat<OneOf, Literal>>.self,
 			"'•' operator isn't optimizing OneOf's properly.")
 	}
+
+	func testPlaygroundExample() throws {
+		let text = """
+		0   0.0   0.01
+		-0   +0   -0.0   +0.0
+		-123.456e+00   -123.456E+00   -123.456e-00   -123.456E-00
+		+123.456e+00   +123.456E+00   +123.456e-00   +123.456E-00
+		0   0.0   0.01
+		-123e+12   -123e-12
+		123.456e+00   123.456E+00
+		0x123E   0x123e
+		0x0123456789abcdef
+		0b0   0b1   0b0000   0b0001   0b11110000   0b0000_1111   0b1010_00_11
+		"""
+
+		let unsigned = digit+
+		let sign = "-" / "+"
+		let integer = Capture(name: "integer", sign¿ • unsigned)
+		let hexa = Capture(name: "hexa", "0x" • hexDigit+)
+		let binary = Capture(name: "binary", "0b" • OneOf("01") • OneOf("01_")*)
+		let floating = Capture(name: "floating", integer • "." • unsigned)
+		let scientific = floating • (("e" / "E") • integer)¿
+		let number = hexa / binary / floating / integer / unsigned / scientific
+
+		let parser = try Parser(search: number)
+
+		XCTAssertEqual(parser.matches(in: text).array().count, 44)
+	}
 }
