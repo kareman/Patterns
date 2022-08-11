8e1cd4ff3064010b559596e408c3c48371a5be14
Kare Morstol <kare@nottoobadsoftware.com>
2022-08-11 20:18:23 +0200

    Use swift-se0270-range-set 1.0.1
    Fixes build failure "Unavailable subscript 'subscript(_:)' was used to satisfy a requirement of protocol 'MutableCollection'" in that package.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
-		.package(url: "https://github.com/apple/swift-se0270-range-set", from: "1.0.0"),
+		.package(url: "https://github.com/apple/swift-se0270-range-set", from: "1.0.1"),
 	],
 	targets: [
 		.target(
